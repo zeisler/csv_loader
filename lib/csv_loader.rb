8998c90d23bab6620ce02a042a89a5a83e46e3f2
Dustin Zeisler <dustin@zeisler.net>
2015-03-07 11:22:35 -0800

    Add ChangeList

--- lib/csv_loader.rb
+++ lib/csv_loader.rb
@@ -1,17 +1,12 @@
 require 'csv'
 require 'csv_loader/version'
-require 'csv_loader/helper'
+require 'csv_loader/helpers'
 require 'csv_loader/abstract'
 require 'csv_loader/each'
+require 'csv_loader/change_list'
 require 'active_support/deprecation'
 
 module CsvLoader
-
-  def self.load(file_path, options = {})
-    Each.new(file_path, options)
-  end
-
   class IdsNotFound < Exception
   end
-
 end
--- spec/csv_loader/each_spec.rb
+++ spec/csv_loader/each_spec.rb
@@ -1,12 +1,12 @@
 require 'rspec'
-require 'csv_loader.rb'
-require 'support/report_type_mock.rb'
+require 'csv_loader'
+require 'support/report_type_mock'
 
 def path(relative)
-  File.join(File.dirname(__FILE__), relative)
+  File.join(File.dirname(__FILE__), '../', relative)
 end
 
-RSpec.describe CsvLoader do
+RSpec.describe CsvLoader::Each do
   after(:each) do
     ActiveMocker::LoadedMocks.clear_all
   end
@@ -17,7 +17,7 @@ RSpec.describe CsvLoader do
 
       describe 'model:' do
         it 'take a csv file and import it into a model' do
-          CsvLoader.load(path('support/report_types.csv'), model: ReportTypeMock)
+          described_class.new(path('support/report_types.csv'), model: ReportTypeMock)
           expect(ReportTypeMock.count).to eq(12)
         end
       end
@@ -25,7 +25,7 @@ RSpec.describe CsvLoader do
       describe 'destroy_all:' do
         it 'has optional argument to destroy all from model' do
           first_row = ReportTypeMock.first
-          CsvLoader.load(path('support/report_types.csv'), model: ReportTypeMock, destroy_all: true)
+          described_class.new(path('support/report_types.csv'), model: ReportTypeMock, destroy_all: true)
           expect(ReportTypeMock.count).to eq(12)
           expect(first_row).not_to eq(ReportTypeMock.first.attributes)
         end
@@ -33,30 +33,30 @@ RSpec.describe CsvLoader do
 
       describe 'where: {condition}' do
         it 'takes a hash condition that will only load csv row that meet it' do
-          CsvLoader.load(path('support/report_types.csv'), model: ReportTypeMock, where: { name: 'My Report 3' })
+          described_class.new(path('support/report_types.csv'), model: ReportTypeMock, where: { name: 'My Report 3' })
           expect(ReportTypeMock.count).to eq 1
           expect(ReportTypeMock.first.attributes).to eq({ "id" => 4, "name" => "My Report 3", "report_family_id" => 1 })
         end
 
         it 'can take more than one condition' do
-          CsvLoader.load(path('support/report_types.csv'), model: ReportTypeMock, where: { name: 'My Report 3', report_family_id: 1 })
+          described_class.new(path('support/report_types.csv'), model: ReportTypeMock, where: { name: 'My Report 3', report_family_id: 1 })
           expect(ReportTypeMock.count).to eq 1
           expect(ReportTypeMock.first.attributes).to eq({ "id" => 4, "name" => "My Report 3", "report_family_id" => 1 })
         end
 
         it 'will return more than one result' do
-          CsvLoader.load(path('support/report_types.csv'), model: ReportTypeMock, where: { report_family_id: 2 })
+          described_class.new(path('support/report_types.csv'), model: ReportTypeMock, where: { report_family_id: 2 })
           expect(ReportTypeMock.count).to eq 5
         end
 
         it 'will take a range of numbers' do
-          CsvLoader.load(path('support/report_types.csv'), model: ReportTypeMock, where: { report_family_id: (1..2) })
+          described_class.new(path('support/report_types.csv'), model: ReportTypeMock, where: { report_family_id: (1..2) })
           expect(ReportTypeMock.count).to eq 12
           expect(ReportTypeMock.all.map(&:report_family_id)).to eq [1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2]
         end
 
         it 'will take a regex' do
-          CsvLoader.load(path('support/report_types.csv'), model: ReportTypeMock, where: { name: /Report 7/ })
+          described_class.new(path('support/report_types.csv'), model: ReportTypeMock, where: { name: /Report 7/ })
           expect(ReportTypeMock.count).to eq 1
           expect(ReportTypeMock.all.map(&:attributes)).to eq([{ "id" => 11, "name" => "My Report 7", "report_family_id" => 2 }])
         end
@@ -64,7 +64,7 @@ RSpec.describe CsvLoader do
 
       describe 'find' do
         it 'takes an array of ids' do
-          CsvLoader.load(path('support/report_types.csv'), model: ReportTypeMock, find: [1, 2, 3])
+          described_class.new(path('support/report_types.csv'), model: ReportTypeMock, find: [1, 2, 3])
           expect(ReportTypeMock.all.map(&:id)).to eq [1, 2, 3]
         end
 
@@ -74,21 +74,19 @@ RSpec.describe CsvLoader do
         end
 
         it 'will raise an error if not found' do
-          expect { CsvLoader.load(path('support/report_types.csv'), model: ReportTypeMock, find: 200) }.to raise_error(CsvLoader::IdsNotFound)
+          expect { described_class.new(path('support/report_types.csv'), model: ReportTypeMock, find: 200) }.to raise_error(CsvLoader::IdsNotFound)
         end
 
         it 'will not raise an error if not found' do
-          expect { CsvLoader.load(path('support/report_types.csv'), model: ReportTypeMock, find: 200, error_if_no_records_found: false) }.not_to raise_error
+          expect { described_class.new(path('support/report_types.csv'), model: ReportTypeMock, find: 200, error_if_no_records_found: false) }.not_to raise_error
         end
       end
 
       describe 'limit' do
-
         it 'it will only load record until limit reached' do
-          CsvLoader.load(path('support/report_types.csv'), model: ReportTypeMock, limit: 3)
+          described_class.new(path('support/report_types.csv'), model: ReportTypeMock, limit: 3)
           expect(ReportTypeMock.all.count).to eq 3
         end
-
       end
 
       describe 'skip_if_exists' do
@@ -101,7 +99,7 @@ RSpec.describe CsvLoader do
           CSV
           temp_file.close
           ReportTypeMock.create(id: 1)
-          CsvLoader.load(temp_file.path, model: ReportTypeMock, skip_if_exists: :id)
+          described_class.new(temp_file.path, model: ReportTypeMock, skip_if_exists: :id)
           expect(ReportTypeMock.count).to eq 2
           expect(ReportTypeMock.all.map(&:id)).to eq [1, 2]
           expect(ReportTypeMock.first.name).to eq nil
@@ -116,7 +114,7 @@ RSpec.describe CsvLoader do
           CSV
           temp_file.close
           ReportTypeMock.create(id: 1, name: 'DC Benchmarking Fees and Services')
-          CsvLoader.load(temp_file.path, model: ReportTypeMock, skip_if_exists: [:id, :name])
+          described_class.new(temp_file.path, model: ReportTypeMock, skip_if_exists: [:id, :name])
           expect(ReportTypeMock.count).to eq 2
           expect(ReportTypeMock.all.map(&:id)).to eq [1, 2]
           expect(ReportTypeMock.first.name).to eq 'DC Benchmarking Fees and Services'
@@ -132,7 +130,7 @@ RSpec.describe CsvLoader do
           temp_file.close
           ReportTypeMock.create(id: 1, name: 'DC Benchmarking Fees and Services')
           ReportTypeMock.create(id: 2)
-          CsvLoader.load(temp_file.path, model: ReportTypeMock, skip_if_exists_any: [:id, :name])
+          described_class.new(temp_file.path, model: ReportTypeMock, skip_if_exists_any: [:id, :name])
           expect(ReportTypeMock.count).to eq 2
           expect(ReportTypeMock.all.map(&:id)).to eq [1, 2]
           expect(ReportTypeMock.last.name).to eq nil
@@ -142,7 +140,7 @@ RSpec.describe CsvLoader do
 
     describe 'case where csv has \N as value' do
       it {
-        CsvLoader.load(path('support/report_types_with_slash_n.csv'),
+        described_class.new(path('support/report_types_with_slash_n.csv'),
                        model: ReportTypeMock)
         expect(ReportTypeMock.first.name).to eq nil
       }
@@ -150,7 +148,7 @@ RSpec.describe CsvLoader do
 
     describe 'freeze' do
       it 'will call freeze on instance after create so that it cannot be modified' do
-        CsvLoader.load(path('support/report_types.csv'), model: ReportTypeMock, find: 1, freeze: true)
+        described_class.new(path('support/report_types.csv'), model: ReportTypeMock, find: 1, freeze: true)
         expect { ReportTypeMock.first.name = 'New Name' }.to raise_error(RuntimeError, /can't modify frozen/)
       end
     end
